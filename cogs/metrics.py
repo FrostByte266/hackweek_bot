d8e0e8449d413e5a763f0247dc487f8924947b6b
FrostByte01 <bjgrigor@gmail.com>
2019-06-27 12:19:55 -0500

    Role counting

--- cogs/metrics.py
+++ cogs/metrics.py
@@ -2,26 +2,27 @@ from discord.ext import commands
 from discord import File
 import matplotlib.pyplot as plt
 
+
 class Metrics(commands.Cog):
 
 	def __init__(self, bot):
 		self.bot = bot
 
-	async def rolesChart(self,ctx):
-		roles = ctx.guild.roles
-		roles_dict = {role:0 for role in roles}
-		#Generate a flat list of all roles currently attached
-		current_tags = [].extend(roles for roles in [member.roles for member in ctx.guild.members])
-		#abusing list comprehension
-		for role in current_tags:
-			roles_dict[role]+=1
-		plot_range = range(len(roles_dict))
+	@commands.command()
+	async def plot(self,ctx):
+		# Create dict of role names and the number of members in each
+		roles_dict = {role: len(role.members) for role in ctx.guild.roles}
+		num_roles = len(roles_dict)
+		# Create plot
+		plot_range = range(num_roles)
+		plt.figure(figsize=(num_roles*2, 10))
 		plt.bar(plot_range, list(roles_dict.values()), align='center')
 		plt.xticks(plot_range, list(roles_dict.keys()))
-		#One roles images per server
-		image_name = f'{ctx.guild.name}{ctx.guild.id}.png'
-		plt.savefig(image_name)
-		await ctx.message.author.send(f'{ctx.guild.name} roles chart', file=File(image_name))
+		# One roles images per server
+		image_path = f'./assets/role_charts/{ctx.guild.id}.png'
+		plt.savefig(image_path)
+		await ctx.message.author.send(f'{ctx.guild.name} roles chart', file=File(image_path))
+
 
 def setup(bot):
 	bot.add_cog(Metrics(bot))
