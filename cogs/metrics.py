c29d662f0c7d6254ad39b5897aa2f68a71974d69
DPR Sanchez <david.p.sanchez@enmu.edu>
2019-06-27 13:36:45 -0600

    added index requirement to dataframe constructor, which is required when scalar values are being passed.

--- cogs/metrics.py
+++ cogs/metrics.py
@@ -20,7 +20,7 @@ class Metrics(commands.Cog):
 		# Create plot
 		plot_range = range(num_roles)
 		data_frame = DataFrame.from_dict(roles_dict).sort_values(ascending=False)
-		data_frame.plot(title=f"{ctx.guild.name} roles on {datetime.today().strftime('%Y-%m-%d')}",kind='bar', width = .2,rot=90,range=plot_range)
+		data_frame.plot(title=f"{ctx.guild.name} roles on {datetime.today().strftime('%Y-%m-%d')}",index=[0],kind='bar', width = .2,rot=90,range=plot_range)
 		# One roles images per server
 		image_path = f'./assets/role_charts/{ctx.guild.id}.png'
 		plt.savefig(image_path)
